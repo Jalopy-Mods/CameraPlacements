2261dad3fead35b14eea92e7443afa8b1bfc704b
MeblIkea <69876322+MeblIkea@users.noreply.github.com>
2023-05-19 11:23:41 +0200

    Now works with JaLoader (before, it was only for BepInEx)

--- obj/Debug/net35/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CameraPlacements
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "CameraPlacements";
-        public const string PLUGIN_NAME = "CameraPlacements";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
